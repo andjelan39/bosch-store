c45ae2973f1d4e1a27f1cf7dfa1d929610273aa6
Andjela <andjelaneskovic39@gmail.com>
2025-07-14 19:03:07 +0200

    Initial commit of backend

---
+++ bosch-backend/src/main/java/com/boschstore/bosch_backend/BoschBackendApplication.java
@@ -0,0 +1,13 @@
+package com.boschstore.bosch_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoschBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoschBackendApplication.class, args);
+	}
+
+}
---
+++ bosch-backend/src/test/java/com/boschstore/bosch_backend/BoschBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boschstore.bosch_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoschBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
